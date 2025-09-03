24adab857e2108ea3dfd6fb403dd5d65f4a23be7
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-03 20:14:41 +0400

    Add many to many and foreignkey connections

--- Lecture code 1/blog_post/blog/models.py
+++ Lecture code 1/blog_post/blog/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 
+class Author(models.Model):
+    first_name = models.CharField(verbose_name='სახელი', max_length=100)
+    last_name = models.CharField(verbose_name='გვარი', max_length=100)
+    email = models.EmailField(verbose_name='მეილი')
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
 class BlogPost(models.Model):
+    authors = models.ManyToManyField(
+        to='blog.Author', verbose_name='ავტორები', related_name='blog_posts')
+
     title = models.CharField(verbose_name="სათაური", max_length=255)
     text = models.TextField(verbose_name="ტექსი")
     active = models.BooleanField(default=True, verbose_name='აქტიურია')
@@ -10,8 +26,8 @@ class BlogPost(models.Model):
     update_date = models.DateTimeField(
         verbose_name="განახლების თარიღი", auto_now=True, null=True)
     website = models.URLField(verbose_name='ვებ მისამართი', null=True)
-    picture = models.ImageField(upload_to='blog_picture/', null=True, blank=True)
     document = models.FileField(upload_to='blog_document/', null=True, blank=True)
+    deleted = models.BooleanField(verbose_name='წაშლილია', default=False)
 
     class Meta:
         verbose_name = "Blog Post"
@@ -23,8 +39,17 @@ class BlogPost(models.Model):
         return self.title
 
 
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+class BlogPostImage(models.Model):
+    blog_post = models.ForeignKey(
+        to='blog.BlogPost',
+        related_name='images',
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='blog_image/')
 
     class Meta:
-        abstract = True
+        verbose_name = "Blog Post Image"
+        verbose_name_plural = "Blog Post Images"
+
+    def __str__(self):
+        return f'{self.blog_post.title} - {self.id} image'
